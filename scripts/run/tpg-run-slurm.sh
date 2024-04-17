5eecaa3399b4930a1ae7842a999c5a03be01d158
skellco <stephen.kelly@dal.ca>
2024-04-17 16:51:42 -0400

    ...

--- scripts/run/tpg-run-slurm.sh
+++ scripts/run/tpg-run-slurm.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=64
 #SBATCH --mem=0
-#SBATCH --time=0-00:20  # time (DD-HH:MM)
+#SBATCH --time=0-03:00  # time (DD-HH:MM)
 
 # # cpus anywhere
 # #SBATCH --ntasks=101               
