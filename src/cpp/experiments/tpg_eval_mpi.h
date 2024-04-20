ce26c72cca7c00d205594ce0207d49a265c9de42
kellys32 <kellys32@mcmaster.ca>
2024-04-20 18:47:52 -0400

    ...

--- src/cpp/experiments/tpg_eval_mpi.h
+++ src/cpp/experiments/tpg_eval_mpi.h
@@ -64,7 +64,8 @@ int WrapDiscreteAction(EvalStruct &eval) {
 }
 
 double WrapContinuousAction(EvalStruct &eval) {
-  return (eval.leafProgram->memGet(memoryEigen::SCALAR_TYPE))->working_memory_[0](0, 0);
+  return (eval.leafProgram->memGet(memoryEigen::SCALAR_TYPE))
+      ->working_memory_[0](0, 0);
 }
 
 vector<team *> GetTeamsToEval(TPG &tpg) {
@@ -327,8 +328,10 @@ void evaluator(TPG &tpg, mpi::communicator &world, vector<TaskEnv *> &tasks) {
         for (eval.episode = 0; eval.episode < eval.tm->_n_eval;
              eval.episode++) {
           tpg._rngs[AUX_SEED_INDEX].seed(eval.episode);
-          // eval.tm->clearMemory(tpg._teamMap);
-          eval.tm->InitMemory(tpg._teamMap);
+          if (tpg.GetParam<double>("p_bid_mu_const") == 0.0)
+            eval.tm->clearMemory(tpg._teamMap);
+          else
+            eval.tm->InitMemory(tpg._teamMap);
           evaluator_map[eval.game->eval_type_](tpg, eval);
           FinalizeStepStats(tpg, eval);
         }
@@ -352,8 +355,10 @@ void replayer(TPG &tpg, vector<TaskEnv *> &tasks) {
     tpg.markEffectiveCode(eval.tm);
     for (eval.episode = 0; eval.episode < eval.tm->_n_eval; eval.episode++) {
       tpg._rngs[AUX_SEED_INDEX].seed(eval.episode);
-      // eval.tm->clearMemory(tpg._teamMap);
-      eval.tm->InitMemory(tpg._teamMap);
+      if (tpg.GetParam<double>("p_bid_mu_const") == 0.0)
+        eval.tm->clearMemory(tpg._teamMap);
+      else
+        eval.tm->InitMemory(tpg._teamMap);
       EvalControl(tpg, eval);
       FinalizeStepStats(tpg, eval);
     }
