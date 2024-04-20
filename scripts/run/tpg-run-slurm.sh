9b86572d90ba6f5146531e96a96c874edfbd92de
kellys32 <kellys32@mcmaster.ca>
2024-04-20 18:38:13 -0400

    ...

--- scripts/run/tpg-run-slurm.sh
+++ scripts/run/tpg-run-slurm.sh
@@ -3,9 +3,9 @@
 
 # single node
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=64
+#SBATCH --ntasks-per-node=40
 #SBATCH --mem=0
-#SBATCH --time=0-12:00  # time (DD-HH:MM)
+#SBATCH --time=0-3:00  # time (DD-HH:MM)
 
 # # cpus anywhere
 # #SBATCH --ntasks=101               
