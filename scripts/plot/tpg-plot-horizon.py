1816ff878ae68377bc893a00c033e983cfbb167d
kellys32 <kellys32@mcmaster.ca>
2024-06-25 17:39:08 -0400

    better times series csv saving

--- scripts/plot/tpg-plot-horizon.py
+++ scripts/plot/tpg-plot-horizon.py
@@ -9,9 +9,11 @@ p = pathlib.Path(file)
 plt.rcParams["figure.figsize"] = [7.50, 3.50]
 plt.rcParams["figure.autolayout"] = True
 
-df = pd.read_csv(p,header=None)
+df = pd.read_csv(p)
 
-df.plot()
-plt.legend(['Target', 'Prediction'])
+# df.plot(plt.plot(df.Name, df.Marks))
+df.plot(x='Time', y=['Target', 'Prediction'])
+# plt.xticks(x, labels, rotation=45)
+plt.legend()#['Target', 'Prediction'])
 plt.ylim(0,1)
 plt.savefig(p.with_name(p.name.split('.')[0]).with_suffix('.png'))
