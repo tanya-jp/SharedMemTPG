794b5780e2861dd56024da87deb24a6cff00ee6a
Eddie Zhuang <zhuane4@mcmaster.ca>
2024-06-26 10:11:35 -0400

    Add experiment creation script

---
+++ scripts/run/create-experiment.sh
@@ -0,0 +1,8 @@
+curl -X POST https://www.comet.com/api/rest/v2/write/experiment/create \
+     -H "Content-Type: application/json" \
+     -H "Authorization: $COMET_API_KEY" \
+     -d '{
+            "workspaceName": "genetic-programming",
+            "projectName": "tpg",
+            "experimentName": "first-experiment"
+        }'
--- src/cpp/TPG/TPG.cc
+++ src/cpp/TPG/TPG.cc
@@ -409,9 +409,7 @@ void TPG::ReadParameters(string file_name,
         outcome_fields[0] == "n_stored_outcomes_VALIDATION" ||
         outcome_fields[0] == "n_stored_outcomes_TEST" ||
         outcome_fields[0] == "forecasting_fitness" ||
-        outcome_fields[0] == "workspace_name" ||
-        outcome_fields[0] == "project_name" ||
-        outcome_fields[0] == "experiment_name") {
+        outcome_fields[0] == "experiment_key") {
       params[outcome_fields[0]] = outcome_fields[1];
     }
     // double parameters are identified by a decimal place
