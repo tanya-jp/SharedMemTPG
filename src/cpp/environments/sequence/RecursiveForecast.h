a2d902fc7b9d1ff2d18027fd58dd94bed223cf8c
kellys32 <kellys32@mcmaster.ca>
2024-06-28 15:35:40 -0400

    ...

--- src/cpp/environments/sequence/RecursiveForecast.h
+++ src/cpp/environments/sequence/RecursiveForecast.h
@@ -107,21 +107,37 @@ class RecursiveForecast : public TaskEnv {
       // 50)
       //   t_start[2].push_back(s);
 
-      uniform_int_distribution<int> DisTrain(
-          0, data.size() - (n_prime_ + n_predict_[0]));
-      uniform_int_distribution<int> DisVal(
-          0, data.size() - (n_prime_ + n_predict_[1]));
 
-      // Train
-      for (int i = 0; i < n_eval_train_; i++)
-        t_start[0].push_back(DisTrain(rng));
 
-      // Validation
+      // //BEST
+      // uniform_int_distribution<int> DisTrain(
+      //     0, data.size() - (n_prime_ + n_predict_[0]));
+      // uniform_int_distribution<int> DisVal(
+      //     0, data.size() - (n_prime_ + n_predict_[1]));
+
+      // // Train
+      // for (int i = 0; i < n_eval_train_; i++)
+      //   t_start[0].push_back(DisTrain(rng));
+
+      // int s = 0;
+      // for (int i = 0; i < n_eval_val_; i++) t_start[0].push_back(s+=100);  
+
+      // // Validation
+      // int s = 0;
+      // for (int i = 0; i < n_eval_val_; i++) t_start[1].push_back(s+=100);
+
+      // // Test
+      // t_start[2] = t_start[1]; // TODO(spkelly): test==val, fix
+
+      
+
+
+
       int s = 0;
-      for (int i = 0; i < n_eval_val_; i++) t_start[1].push_back(s+=100);
+      for (int i = 0; i < n_eval_val_; i++) t_start[0].push_back(s+=100);  
 
-      // Test
-      t_start[2] = t_start[1]; // TODO(spkelly): test==val, fix
+      t_start[1] = t_start[0];
+      t_start[2] = t_start[0]; 
     }
     cout << "time series train slices: " << t_start[0].size() << endl;
     cout << "time series validation slices: " << t_start[1].size() << endl;
