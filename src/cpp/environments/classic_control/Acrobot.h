f48d24c4359de6aef44c0be3be7effbb7ec4de2e
Mark Cruz <markaycruz@gmail.com>
2024-10-27 22:33:01 -0400

    Format `Acrobot`, `CartPole`, and `CartCentering` (#126)
    * Format files
    
    * fix Acrobot
    
    * Add formatting changes
    
    * Add more formatting changes
    
    * Add new changes
    
    * fix changes

--- src/cpp/environments/classic_control/Acrobot.h
+++ src/cpp/environments/classic_control/Acrobot.h
@@ -1,98 +1,103 @@
-#ifndef Acrobot_h
-#define Acrobot_h
+#ifndef ACROBOT_H_
+#define ACROBOT_H_
 
-#include <ClassicControlEnv.h>
-#include <math.h>
-#include <stdlib.h>
-
+#include <cmath>
+#include <cstdlib>
 #include <iostream>
+#include <random>
+
+#include "ClassicControlEnv.h"
 
 #if !defined(CCANADA) && !defined(HPCC)
 #include <GL/gl.h>
 #include <GL/glut.h>
 #endif
 
-#define STATE_SIZE 4
-
-using namespace std;
+constexpr int kAcrobotStateSize = 4;
 
 class Acrobot : public ClassicControlEnv {
    protected:
-    const double maxTheta1 = M_PI;
-    const double maxTheta2 = M_PI;
-    const double maxTheta1Dot = 4 * M_PI;
-    const double maxTheta2Dot = 9 * M_PI;
-    const double m1 = 1.0;
-    const double m2 = 1.0;
-    const double l1 = 1.0;
-    const double l2 = 1.0;
-    const double lc1 = 0.5;
-    const double lc2 = 0.5;
-    const double I1 = 1.0;
-    const double I2 = 1.0;
-    const double g = 9.8;
-    const double dt = 0.05;
-    const double AcrobotGoalPosition = 1.0;
+    static constexpr double kMaxTheta1 = M_PI;
+    static constexpr double kMaxTheta2 = M_PI;
+    static constexpr double kMaxTheta1Dot = 4 * M_PI;
+    static constexpr double kMaxTheta2Dot = 9 * M_PI;
+    static constexpr double kM1 = 1.0;
+    static constexpr double kM2 = 1.0;
+    static constexpr double kL1 = 1.0;
+    static constexpr double kLC1 = 0.5;
+    static constexpr double kLC2 = 0.5;
+    static constexpr double kI1 = 1.0;
+    static constexpr double kI2 = 1.0;
+    static constexpr double kG = 9.8;
+    static constexpr double kDt = 0.05;
+    static constexpr double kAcrobotGoalPosition = 1.0;
 
-    // state array indexing
-    const int _theta1 = 0;
-    const int _theta2 = 1;
-    const int _theta1Dot = 2;
-    const int _theta2Dot = 3;
-
-    const int _theta1_po = 0;
-    const int _theta2_po = 1;
+    // State array indexing
+    enum StateIndex {
+        kTheta1 = 0,
+        kTheta2 = 1,
+        kTheta1Dot = 2,
+        kTheta2Dot = 3
+    };
+    enum StatePoIndex { kTheta1Po = 0, kTheta2Po = 1 };
 
    public:
     Acrobot() {
         n_eval_train_ = 20;
         n_eval_validation_ = 0;
         n_eval_test_ = 100;
-        disReset = uniform_real_distribution<>(-0.1, 0.1);
+        disReset = std::uniform_real_distribution<>(-0.1, 0.1);
         actionsDiscrete.push_back(-1.0);
         actionsDiscrete.push_back(0.0);
         actionsDiscrete.push_back(1.0);
         eval_type_ = "Control";
         max_step_ = 200;
-        state_.reserve(STATE_SIZE);
-        state_.resize(STATE_SIZE);
-        state_po_.reserve(STATE_SIZE);
-        state_po_.resize(STATE_SIZE);
+        state_.reserve(kAcrobotStateSize);
+        state_.resize(kAcrobotStateSize);
+        state_po_.reserve(kAcrobotStateSize);
+        state_po_.resize(kAcrobotStateSize);
     }
 
     ~Acrobot() {}
 
+    // TODO: Change function name once TaskEnv follows Google's C++ Styling
     bool discreteActions() const { return false; }
 
+    // TODO: Change function name once TaskEnv follows Google's C++ Styling
     double minActionContinuous() const { return -1.0; }
 
+    // TODO: Change function name once TaskEnv follows Google's C++ Styling
     double maxActionContinuous() const { return 1.0; }
 
-    void normalizeState(bool po) {
+    void NormalizeState(bool po) {
         if (po) {
-            state_po_[_theta1] /= maxTheta1;
-            state_po_[_theta2] /= maxTheta2;
+            state_po_[StateIndex::kTheta1] /= kMaxTheta1;
+            state_po_[StateIndex::kTheta2] /= kMaxTheta2;
         }
     }
 
-    void reset(mt19937 &rng) {
-        state_po_[_theta1] = state_[_theta1] = disReset(rng);
+    // TODO: Change function name once TaskEnv follows Google's C++ Styling
+    void reset(std::mt19937 &rng) {
+        state_po_[StateIndex::kTheta1] = state_[StateIndex::kTheta1] =
+            disReset(rng);
 
-        state_po_[_theta2] = state_[_theta2] = disReset(rng);
+        state_po_[StateIndex::kTheta2] = state_[StateIndex::kTheta2] =
+            disReset(rng);
 
-        state_[_theta1Dot] = disReset(rng);
+        state_[StateIndex::kTheta1Dot] = disReset(rng);
 
-        state_[_theta2Dot] = disReset(rng);
+        state_[StateIndex::kTheta2Dot] = disReset(rng);
 
         reward = 0;
 
         step_ = 0;
         terminalState = false;
 
-        normalizeState(true);
+        NormalizeState(true);
     }
 
-    Results update(int actionD, double actionC, mt19937 &rng) {
+    // TODO: Change function name once TaskEnv follows Google's C++ Styling
+    Results update(int actionD, double actionC, std::mt19937 &rng) {
         (void)actionD;
         (void)rng;
 
@@ -110,70 +115,83 @@ class Acrobot : public ClassicControlEnv {
         while (!terminal() && count < 4) {
             count++;
 
-            d1 = m1 * pow(lc1, 2) +
-                 m2 * (pow(l1, 2) + pow(lc2, 2) +
-                       2 * l1 * lc2 * cos(state_[_theta2])) +
-                 I1 + I2;
-            d2 = m2 * (pow(lc2, 2) + l1 * lc2 * cos(state_[_theta2])) + I2;
+            d1 =
+                kM1 * std::pow(kLC1, 2) +
+                kM2 * (std::pow(kL1, 2) + std::pow(kLC2, 2) +
+                       2 * kL1 * kLC2 * std::cos(state_[StateIndex::kTheta2])) +
+                kI1 + kI2;
+            d2 = kM2 * (std::pow(kLC2, 2) +
+                        kL1 * kLC2 * std::cos(state_[StateIndex::kTheta2])) +
+                 kI2;
 
-            phi_2 = m2 * lc2 * g *
-                    cos(state_[_theta1] + state_[_theta2] - M_PI / 2.0);
-            phi_1 =
-                -(m2 * l1 * lc2 * pow(state_[_theta2Dot], 2) *
-                      sin(state_[_theta2]) -
-                  2 * m2 * l1 * lc2 * state_[_theta1Dot] * state_[_theta2Dot] *
-                      sin(state_[_theta2])) +
-                (m1 * lc1 + m2 * l1) * g * cos(state_[_theta1] - M_PI / 2.0) +
-                phi_2;
+            phi_2 = kM2 * kLC2 * kG *
+                    std::cos(state_[StateIndex::kTheta1] +
+                             state_[StateIndex::kTheta2] - M_PI / 2.0);
+            phi_1 = -(kM2 * kL1 * kLC2 *
+                          std::pow(state_[StateIndex::kTheta2Dot], 2) *
+                          std::sin(state_[StateIndex::kTheta2]) -
+                      2 * kM2 * kL1 * kLC2 * state_[StateIndex::kTheta1Dot] *
+                          state_[StateIndex::kTheta2Dot] *
+                          std::sin(state_[StateIndex::kTheta2])) +
+                    (kM1 * kLC1 + kM2 * kL1) * kG *
+                        std::cos(state_[StateIndex::kTheta1] - M_PI / 2.0) +
+                    phi_2;
 
-            theta2_ddot = (torque + (d2 / d1) * phi_1 -
-                           m2 * l1 * lc2 * pow(state_[_theta1Dot], 2) *
-                               sin(state_[_theta2]) -
-                           phi_2) /
-                          (m2 * pow(lc2, 2) + I2 - pow(d2, 2) / d1);
+            theta2_ddot =
+                (torque + (d2 / d1) * phi_1 -
+                 kM2 * kL1 * kLC2 *
+                     std::pow(state_[StateIndex::kTheta1Dot], 2) *
+                     std::sin(state_[StateIndex::kTheta2]) -
+                 phi_2) /
+                (kM2 * std::pow(kLC2, 2) + kI2 - std::pow(d2, 2) / d1);
 
             theta1_ddot = -(d2 * theta2_ddot + phi_1) / d1;
 
-            state_[_theta1Dot] += theta1_ddot * dt;
-            state_[_theta2Dot] += theta2_ddot * dt;
+            state_[StateIndex::kTheta1Dot] += theta1_ddot * kDt;
+            state_[StateIndex::kTheta2Dot] += theta2_ddot * kDt;
 
-            state_[_theta1] += state_[_theta1Dot] * dt;
-            state_[_theta2] += state_[_theta2Dot] * dt;
-            state_[_theta1] = wrap(state_[_theta1], -maxTheta1, maxTheta1);
-            state_[_theta2] = wrap(state_[_theta2], -maxTheta2, maxTheta2);
-            state_[_theta1Dot] =
-                bound(state_[_theta1Dot], -maxTheta1Dot, maxTheta1Dot);
-            state_[_theta2Dot] =
-                bound(state_[_theta2Dot], -maxTheta2Dot, maxTheta2Dot);
+            state_[StateIndex::kTheta1] += state_[StateIndex::kTheta1Dot] * kDt;
+            state_[StateIndex::kTheta2] += state_[StateIndex::kTheta2Dot] * kDt;
+            state_[StateIndex::kTheta1] =
+                Wrap(state_[StateIndex::kTheta1], -kMaxTheta1, kMaxTheta1);
+            state_[StateIndex::kTheta2] =
+                Wrap(state_[StateIndex::kTheta2], -kMaxTheta2, kMaxTheta2);
+            state_[StateIndex::kTheta1Dot] = bound(
+                state_[StateIndex::kTheta1Dot], -kMaxTheta1Dot, kMaxTheta1Dot);
+            state_[StateIndex::kTheta2Dot] = bound(
+                state_[StateIndex::kTheta2Dot], -kMaxTheta2Dot, kMaxTheta2Dot);
         }
 
-        state_po_[_theta1] = state_[_theta1];
-        state_po_[_theta2] = state_[_theta2];
+        state_po_[StateIndex::kTheta1] = state_[StateIndex::kTheta1];
+        state_po_[StateIndex::kTheta2] = state_[StateIndex::kTheta2];
 
         step_++;
 
         reward = -1.0;
 
-        normalizeState(true);
+        NormalizeState(true);
         return {reward, 0.0};
     }
 
+    // TODO: Change function name once TaskEnv follows Google's C++ Styling
     bool terminal() {
-        if (step_ >= max_step_ ||
-            (-cos(state_[_theta1]) - cos(state_[_theta2] + state_[_theta1]) >
-             AcrobotGoalPosition))
+        if (step_ >= max_step_ || (-std::cos(state_[StateIndex::kTheta1]) -
+                                       std::cos(state_[StateIndex::kTheta2] +
+                                                state_[StateIndex::kTheta1]) >
+                                   kAcrobotGoalPosition))
             terminalState = true;
         return terminalState;
     }
 
-    double wrap(double x, double m, double M) {
+    double Wrap(double x, double m, double M) {
         double diff = M - m;
         while (x > M) x = x - diff;
         while (x < m) x = x + diff;
         return x;
     }
 
-    // opengl
+    // TODO: Change function name once TaskEnv follows Google's C++ Styling
+    // OpenGL Display
     void display_function(int episode, int actionD, double actionC) {
         (void)episode;
         (void)actionD;
@@ -186,15 +204,17 @@ class Acrobot : public ClassicControlEnv {
 
         glLineWidth(5.0);
 
-        x2 = r1 * cos(M_PI / 2 - state_[_theta1]);
-        y2 = r1 * sin(M_PI / 2 - state_[_theta1]);
+        x2 = r1 * std::cos(M_PI / 2 - state_[StateIndex::kTheta1]);
+        y2 = r1 * std::sin(M_PI / 2 - state_[StateIndex::kTheta1]);
         glColor3f(1.0, 1.0, 1.0);
         glBegin(GL_LINES);
         glVertex2d(0.0, 0.0);
         glVertex2d(-x2, -y2);
 
-        x3 = x2 + r2 * cos(M_PI / 2 - (state_[_theta1] + state_[_theta2]));
-        y3 = y2 + r2 * sin(M_PI / 2 - (state_[_theta1] + state_[_theta2]));
+        x3 = x2 + r2 * std::cos(M_PI / 2 - (state_[StateIndex::kTheta1] +
+                                            state_[StateIndex::kTheta2]));
+        y3 = y2 + r2 * std::sin(M_PI / 2 - (state_[StateIndex::kTheta1] +
+                                            state_[StateIndex::kTheta2]));
         glColor3f(1.0, 1.0, 1.0);
         glBegin(GL_LINES);
         glVertex2d(-x2, -y2);
@@ -224,11 +244,11 @@ class Acrobot : public ClassicControlEnv {
 
         char c[80];
         if (step_ == 0)
-            sprintf(c, "Acrobot Initial Conditions%s", ":");
+            std::sprintf(c, "Acrobot Initial Conditions%s", ":");
         else if (terminal())
-            sprintf(c, "Acrobot Terminal%s", ":");
+            std::sprintf(c, "Acrobot Terminal%s", ":");
         else
-            sprintf(c, "Acrobot%s", ":");
+            std::sprintf(c, "Acrobot%s", ":");
         drawStrokeText(c, -1.9, -1.7, 0);
 
         glFlush();
@@ -236,4 +256,4 @@ class Acrobot : public ClassicControlEnv {
     }
 };
 
-#endif
+#endif  // ACROBOT_H
