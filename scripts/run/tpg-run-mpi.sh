1eee29934625c75fa5dc7e8c5b603261e4cfaf35
kellys32 <kellys32@mcmaster.ca>
2024-10-29 22:19:51 -0400

    ...

--- scripts/run/tpg-run-mpi.sh
+++ scripts/run/tpg-run-mpi.sh
@@ -35,7 +35,7 @@ fi
 # Replay mode ##################################################################
 if [ $mode -eq 1 ]; then
    # Training phase
-   phase=2
+   phase=0
    if ls replay/frames/* 1> /dev/null 2>&1; then rm replay/frames/*; fi
    if ls rplay/graphs/* 1> /dev/null 2>&1; then rm replay/graphs/*; fi
    if ls rplay/graphs/* 1> /dev/null 2>&1; then rm replay/graphs/*; fi
