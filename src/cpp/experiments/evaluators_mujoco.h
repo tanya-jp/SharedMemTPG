53abdbb45598ebed079190ddcffb9c18eaa06045
kellys32 <kellys32@mcmaster.ca>
2024-11-26 15:16:50 -0500

    rm dbg output

--- src/cpp/experiments/evaluators_mujoco.h
+++ src/cpp/experiments/evaluators_mujoco.h
@@ -173,8 +173,6 @@ void EvalMujoco(TPG& tpg, EvalData& eval) {
         eval.n_prediction++;
         obs->Set(task->GetObsVec(eval.partially_observable));
         MaybeAnimateStep(tpg);
-
-        // cerr << vecToStr(task->GetObsVec(eval.partially_observable)) << endl;
     }
     delete obs;
 }
