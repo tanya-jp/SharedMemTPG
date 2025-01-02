8acabf07218162d461af198431c6c15d84f78f20
skellco <stephen.kelly@dal.ca>
2025-01-02 13:23:07 -0500

    ...

--- experiment_directories/mujoco-Reacher/tpg-run-slurm.sh
+++ experiment_directories/mujoco-Reacher/tpg-run-slurm.sh
@@ -2,31 +2,31 @@
 #SBATCH --account=def-skelly
 
 # cpus anywhere
-#SBATCH --ntasks=51               
+#SBATCH --ntasks=11               
 #SBATCH --mem-per-cpu=4G      
-#SBATCH --time=0-12:00  # time (DD-HH:MM)
+#SBATCH --time=0-00:20  # time (DD-HH:MM)
 
 #defaults
 mode=0 #Train:0, Replay:1, Debug:2
-seed=1
-parameters_file="parameters.txt"
+seed_tpg=42
+parameters_file="parameters_TPG.txt"
 
 while getopts m:p:s: flag
 do
    case "${flag}" in
       m) mode=${OPTARG};;
       p) parameters_file=${OPTARG};;
-      s) seed=${OPTARG};;
+      s) seed_tpg=${OPTARG};;
    esac
 done
 
 # Start from scratch ###########################################################
 if [ $mode -eq 0 ]; then
   srun $TPG/build/release/cpp/experiments/TPGExperimentMPI \
-  parameters_file=$parameters_file \
-  seed_tpg=$seed \
-  1> tpg.$seed.$$.std \
-  2> tpg.$seed.$$.err
+  parameters_file=${parameters_file} \
+  seed_tpg=${seed_tpg} \
+  1> tpg.${seed_tpg}.$$.std \
+  2> tpg.${seed_tpg}.$$.err
 fi
 
 # Pickup from checkpoint #######################################################
@@ -36,7 +36,7 @@ if [ $mode -eq 4 ]; then
   checkpoints/cp.*.${seed_tpg}.${checkpoint_in_phase}.rslt | \
   cut -d '.' -f 2 | sort -n | tail -n 1)
   pid=$(ls tpg.${seed_tpg}.*.std | cut -d '.' -f 3 | tail -n 1)
-  echo "Starting run ${seed_tpg} t ${checkpoint_in_t} pid $pid"
+  echo "Starting run ${seed_tpg} t ${checkpoint_in_t} pid $pid" > oot
   srun $TPG/build/release/cpp/experiments/TPGExperimentMPI \
     parameters_file=${parameters_file} \
     seed_tpg=${seed_tpg} \
