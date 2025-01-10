5c68bbd65fa449c1ae3d485d1f754f8c709dbdd6
kellys32 <kellys32@mcmaster.ca>
2025-01-10 15:05:16 -0500

    ...

--- experiment_directories/mujoco-Reacher/tpg-run-slurm.sh
+++ experiment_directories/mujoco-Reacher/tpg-run-slurm.sh
@@ -3,9 +3,9 @@
 
 # cpus anywhere
 # #SBATCH --nodes=1
-#SBATCH --ntasks=201               
+#SBATCH --ntasks=101               
 #SBATCH --mem-per-cpu=4G      
-#SBATCH --time=0-03:00  # time (DD-HH:MM)
+#SBATCH --time=0-12:00  # time (DD-HH:MM)
 
 #defaults
 mode=0 #Train:0, Replay:1, Debug:2
