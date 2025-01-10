aaf6be5f235321b75a7960ae2fc3fc78e9c4f800
djavahet <djavahet@mcmaster.ca>
2025-01-10 09:07:51 -0500

    Add new DS

--- src/cpp/TPG/sharedMemoryEigen.h
+++ src/cpp/TPG/sharedMemoryEigen.h
@@ -1,67 +1,82 @@
 #ifndef sharedMemoryEigen_h
 #define sharedMemoryEigen_h
 
-
 #include <Eigen/Dense>
-#include <cmath>
-#include <iostream> 
-#include <iomanip>  
+#include <vector>
+#include "MemoryEigen.h"
 
-class sharedMemoryEigen {
+class sharedMemoryEigen
+{
+private:
+    /* data */
 public:
-    // Constructor
-    sharedMemoryEigen(int rows=30, int cols=8){
-        rows_ = rows;
-        cols_ = cols;
-        team_memory_.resize(rows, cols); 
-    }
-    
-    // Destructor
-    // ~sharedMemoryEigen(){}
 
-    // Function to print the entire team_memory_ matrix
-    void printTeamMemory() const {
-        std::cout << "Team Memory Matrix (" << rows_ << "x" << cols_ << "):\n";
-        for (int i = 0; i < team_memory_.rows(); ++i) {
-            for (int j = 0; j < team_memory_.cols(); ++j) {
-                std::cout << std::setw(8) << std::fixed << std::setprecision(2)
-                          << team_memory_(i, j) << " ";
-            }
-            std::cout << "\n";
-        }
-    }
-    
-    // Function to set values in the Eigen matrix (optional, depending on needs)
-    void setMatrixValue(int row, int col, double value) {
-        if (row >= 0 && row < static_cast<int>(rows_) && 
-            col >= 0 && col < static_cast<int>(cols_)) {
-            team_memory_(row, col) = value;
-        }
-        else{
-            cout << "ERROR: col " << col << "row " << row << endl;
-        }
-    }
+    // Vector of MemoryEigen objects
+    std::vector<MemoryEigen *> team_scalar_memory_;
+    std::vector<MemoryEigen *> team_vector_memory_;
+    std::vector<MemoryEigen *> team_matrix_memory_;
 
-    // Probability functions
-    double memWriteProb_def(int i) const {
-        return 0.25 - std::pow(0.01 * i, 2);
-    }
-
-    double memWriteProb_cauchy1(int i) const {
-        return 1.0 / (M_PI * (std::pow(i, 2) + 1));
-    }
-
-    double memWriteProb_cauchyHalf(int i) const {
-        return 0.25 / (0.5 * M_PI * (std::pow(i, 2) + 0.25));
-    }
-    
-    Eigen::MatrixXd team_memory_; // Pointer to Eigen Matrix to manage memory
     static const size_t NA_TYPE = 3;
     static const size_t SHARED_MEM_TYPE = 4;
     static const int NUM_SHARED_MEM_TYPES = 1;
 
     size_t rows_;
     size_t cols_;
+
+    sharedMemoryEigen(int rows=10, int cols=8){
+        rows_ = rows;
+        cols_ = cols;
+
+        // // Add scalar, vector, and matrix MemoryEigen
+        // MemoryEigen scalarMemory(MemoryEigen::kScalarType_, 1, 1);
+        // MemoryEigen vectorMemory(MemoryEigen::kVectorType_, 3, 3); // 3 indices, 3x1 vectors
+        // MemoryEigen matrixMemory(MemoryEigen::kMatrixType_, 2, 2); // 2 indices, 2x2 matrices
+
+        // team_scalar_memory_.push_back(scalarMemory);
+        
+
+        // Add to memories vectors
+        for (int i=0; i<= 8; i++){
+
+            team_scalar_memory_.push_back(
+                new MemoryEigen (MemoryEigen::kScalarType_, 1, 1));
+            team_vector_memory_.push_back(
+                new MemoryEigen (MemoryEigen::kVectorType_, 3, 3));
+            team_matrix_memory_.push_back(
+                new MemoryEigen (MemoryEigen::kMatrixType_, 2, 2));
+        }
+    }
+
+
+    // ~sharedMemoryEigen();
+
+    // // Add a new MemoryEigen object to the vector
+    // void AddMemory(const MemoryEigen& memory) {
+    //     team_memory_.push_back(memory);
+    // }
+
+    // // Clear all memories
+    // void ClearMemories() {
+    //     team_scalar_memory_.clear();
+    //     team_vector_memory_.clear();
+    //     team_matirx_memory_.clear();
+    // }
+
+    // // Get a reference to a specific MemoryEigen object
+    // MemoryEigen& GetMemory(size_t index) {
+    //     if (index >= team_memory_.size()) {
+    //         throw std::out_of_range("Index out of range in GetMemory.");
+    //     }
+    //     return team_memory_[index];
+    // }
+
+    // // Print details of all MemoryEigen objects
+    // void PrintMemories() const {
+    //     for (size_t i = 0; i < team_memory_.size(); ++i) {
+    //         std::cout << "Memory " << i << ":\n";
+    //         std::cout << team_memory_[i].ToString(0) << "\n"; // Assuming prog_id = 0 for demo
+    //     }
+    // }
 };
 
 #endif // sharedMemoryEigen_h
