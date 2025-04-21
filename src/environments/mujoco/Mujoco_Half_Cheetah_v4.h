13a9cad24df92b7c9a774c5ec5e4dec046df9278
Stephen Kelly <kellys32@mcmaster.ca>
2025-04-21 12:04:52 -0400

    Skdev

--- src/environments/mujoco/Mujoco_Half_Cheetah_v4.h
+++ src/environments/mujoco/Mujoco_Half_Cheetah_v4.h
@@ -23,6 +23,7 @@ class Mujoco_Half_Cheetah_v4 : public MujocoEnv {
       model_path_ =
           ExpandEnvVars(std::any_cast<string>(params["mj_model_path"]) + 
                         "half_cheetah.xml");
+      frame_skip_ = 5;                  
       initialize_simulation();
       obs_size_ = 17;
       if (!exclude_current_positions_from_observation_)
